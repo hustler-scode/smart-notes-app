5271b3e1521da0388c4ea46308bf50f932dd0999
hustler-scode <kalukids16@gmail.com>
2026-03-05 12:03:29 +0300

    updated frontend api link

--- backend/server.js
+++ backend/server.js
@@ -45,9 +45,13 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 --------------------------------------------------
 */
 
+const PORT = process.env.PORT || 5000;
+
 sequelize.sync({ alter: true })
   .then(() => {
     console.log("Database synced");
-    app.listen(5000, () => console.log("Server running on port 5000"));
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
   })
   .catch(err => console.error(err));
